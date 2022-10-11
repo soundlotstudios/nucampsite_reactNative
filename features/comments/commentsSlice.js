f3d735edfe6ec74bde64ff0bc9ba521733ece9d0
Steven Olmos <eaodesigns@gmail.com>
2022-10-10 18:50:50 -0700

    Week 2 workshop + Favorites

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -9,10 +9,27 @@ export const fetchComments = createAsyncThunk(
   }
 );
 
+export const postComment = createAsyncThunk(
+  "comments/postComment",
+  async (payload, { dispatch, getState }) => {
+    setTimeout(() => {
+      const { comments } = getState();
+      payload.date = new Date().toISOString();
+      payload.id = comments.commentsArray.length;
+      // console.log("Payload", payload);
+      dispatch(addComment(payload));
+    }, 2000);
+  }
+);
+
 const commentsSlice = createSlice({
   name: "comments",
   initialState: { isLoading: true, errMess: null, commentsArray: [] },
-  reducers: {},
+  reducers: {
+    addComment: (state, action) => {
+      state.commentsArray.push(action.payload);
+    },
+  },
   extraReducers: {
     [fetchComments.pending]: (state) => {
       state.isLoading = true;
@@ -29,4 +46,5 @@ const commentsSlice = createSlice({
   },
 });
 
+export const { addComment } = commentsSlice.actions;
 export const commentsReducer = commentsSlice.reducer;
